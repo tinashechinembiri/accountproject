5823bf133b033a7e88532e0b7ef6bc0825cd5b34
tinashechinembiri <tinashechinembiri5@gmail.com>
2018-11-13 17:54:01 +0000

    secontest

--- src/main/java/Domain/Service.java
+++ src/main/java/Domain/Service.java
@@ -10,6 +10,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class Service {
 	Map<Integer, BankApplication> name = new HashMap<Integer, BankApplication>();
 	BankApplication m1 = new BankApplication("jo", "mario", "1");
+	
+int count =  0; 	
+	
 	public static void main(String[] arg) throws JsonProcessingException {
 		Service m1 = new Service();
 		m1.Details();
@@ -41,14 +44,22 @@ public class Service {
 			e.printStackTrace();
 		}
 	}
+	
 
+public void add_member (BankApplication name1 )
+{
+	this.name.put(count, name1); 
+	
+	count++; 
+}
 public int get_person (String name1)
 {
-	int counter = 0; 
+	 int counter = 0;
+	 
 	
 	for (BankApplication check : name.values()  )
 	{
-		if (check.getLastName().equals(name1))
+		if (check.getFirstName().equals(name1))
 		{
 			counter ++; 
 		}
@@ -56,6 +67,10 @@ public int get_person (String name1)
 	
 	return counter ; 
 	
+}
+public int stream_person(String name )
+{
+	return 0; 
 }
 
 }
