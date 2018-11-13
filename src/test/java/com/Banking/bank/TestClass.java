7f6117a37333fd085c18c315df8c946548e7d537
tinashechinembiri <tinashechinembiri5@gmail.com>
2018-11-13 19:00:20 +0000

    lastcheck

--- src/test/java/com/Banking/bank/TestClass.java
+++ src/test/java/com/Banking/bank/TestClass.java
@@ -2,7 +2,8 @@ package com.Banking.bank;
 
 import static org.junit.Assert.*;
 
-
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -14,6 +15,7 @@ import Domain.Service;
 
 public class TestClass {
 	
+	Map<Integer, BankApplication> name = new HashMap<Integer, BankApplication>();
 	@Before 
 	public void initialize()
 	{
@@ -61,26 +63,7 @@ public class TestClass {
 		assertEquals(3, thisservice.stream_person("jo"));
 		assertEquals(1, thisservice.stream_person("jamal"));
 	}
-	@Test
-	public void test3() {
-		Service thisservice = new Service(); 
-		
-		BankApplication m1 = new BankApplication ("jo", "mario", "1");
-		
-	
-	
-		BankApplication m2 = new BankApplication (); 
-		;
-		
-		thisservice.add_member(m1);
-		thisservice.add_member(m2);
-		
-		
-	//	assertEquals("Tinashe", m1.getFirstName());
-	
-		
-		
-	}
+
 
 
 }
